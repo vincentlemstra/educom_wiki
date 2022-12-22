7ff88cdb75eb0ce6a583e9e9be13704ef9075641
KoenvRijswijk <121175630+KoenvRijswijk@users.noreply.github.com>
2022-12-22 12:51:10 +0100

    Add files via upload

---
+++ tests/koen/test_form_multi/siteDAO.php
@@ -0,0 +1,131 @@
+<?php
+class SiteDAO 
+{
+//==============================================================================
+// TO DO SELECT FIELDINFO FROM DATABASE
+//==============================================================================
+    public function getFormInfoByPage(string $page) : array
+    {
+        $submittxt = [
+            'contact' => 'Versturen',  
+            'login' => 'Inloggen',  
+            'register' => 'Registreren',
+            'search'  => 'Zoek artikelen'
+        ];
+        return [
+            'page' => $page,
+            'action' => '',
+            'method' => 'POST',
+            'submit' => Tools::getValueFromArray($page,$submittxt,'Bewaar')
+        ];
+    }    
+//==============================================================================
+// TO DO SELECT FIELDINFO FROM DATABASE
+//==============================================================================
+    public function getFieldInfoByPage(string $page) : array
+    {
+        switch ($page)
+        {
+            case 'contact' :
+                return [
+                    'name' => [
+                        'type' => 'text', 		
+                        'label'=> 'Je naam',
+                        'placeholder' => 'Vul hier je naam in',
+                        //'default' => //Tools::getSesVar(USERNAME)
+                    ],		
+                    'email' => [
+                        'type' => 'email',
+                        'label'=> 'Je email-adres',
+                        'placeholder' => 'Vul hier je email-adres in',
+                        'check_func' => 'validEmail',
+                        //'default' => //Tools::getSesVar(USEREMAIL)
+                    ],	
+                    'phone' =>  [
+                        'type' => 'tel',
+                        'label'=> 'Je telefoonnummer',
+                        'placeholder' => 'Vul hier je telefoonnummer in',
+                    ],	
+                    'message' => [
+                        'type' => 'textarea',
+                        'label'=> 'Je bericht',
+                        'placeholder' => 'Vul hier je bericht in'
+                    ],		
+                    'contact' =>  [
+                        'type' => 'select',
+                        'label'=> 'Neem contact met op via',
+                        'options' => [
+                            'Email' => 'contact_email',
+                            'Telefoon' => 'contact_phone',
+                            'Postduif'  => 'contact_pidgeon'
+                        ]
+                    ]
+                ];		
+            case 'login' :
+                return [
+                    'email' => [
+                        'type' => 'email',
+                        'label'=> 'Je email-adres',
+                        'placeholder' => 'Vul hier je email-adres in',
+                        'check_func' => 'validEmail',
+                        //'default' => Tools::getSesVar(USEREMAIL)
+                    ],	
+                    'password' => [
+                        'type' => 'password', 		
+                        'label'=> 'Je wachtwoord',
+                        'placeholder' => 'Vul hier je wachtwoord in'
+                    ]
+                ];    
+            case 'register' :
+                return [
+                    'name' => [
+                        'type' => 'text', 		
+                        'label'=> 'Je naam',
+                        'placeholder' => 'Vul hier je naam in',
+                    ],		
+                    'email' => [
+                        'type' => 'email',
+                        'label'=> 'Je email-adres',
+                        'placeholder' => 'Vul hier je email-adres in',
+                        'check_func' => 'validEmail'
+                    ],	
+                    'password' => [
+                        'type' => 'password', 		
+                        'label'=> 'Je wachtwoord',
+                        'placeholder' => 'Vul hier je wachtwoord in'
+                    ],
+                    'rep_password' => [
+                        'type' => 'password', 		
+                        'label'=> 'Herhaal je wachtwoord',
+                        'placeholder' => 'Vul hier je wachtwoord in'
+                    ]
+                ];
+             case 'search' :
+                return [
+                    'auteur' => [
+                        'type' => 'select multiple',
+                        'label'=> 'Selecteer auteur:',
+                         'options' => [
+                            'Jordi' => 'Jordi',
+                            'Koen' => 'Koen',
+                            'Yoeri' => 'Yoeri',
+                            'Vincent'  => 'Vincent'
+                        ]
+                    ],  
+                    'tag' => [
+                        'type' => 'select multiple',       
+                        'label'=> 'Selecteer Tag:',
+                         'options' => [
+                            'Javascript' => 'Javascript',
+                            'PHP' => 'PHP',
+                            'Ajax' => 'Ajax',
+                            'MYSQL'  => 'MYSQL'
+                        ]
+                    ]
+                ];    
+            default :
+                // Show Error!!!
+                return [];
+        }        
+    }
+}
